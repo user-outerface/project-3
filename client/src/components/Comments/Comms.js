ff66c4424b4ca75879b5a2c44782b1084c3f5b56
jsbryce1 <jedi1991@hotmail.com>
2018-10-04 13:33:42 -0600

    modal and color changes

--- client/src/components/Comments/Comms.js
+++ client/src/components/Comments/Comms.js
@@ -7,7 +7,7 @@ export const Comms = props => {
     <div className=" mb-3 my-1 comms-div-p">
         <div className="text-left comms-div">
             <div className="align-self-center comms-text">{props.children}</div>
-                <Button className="btn comms-btn " onClick={props.onClickPass} attribsext={{ id: props.id }}>ಠ_ಠ  &#10157;</Button>
+                <Button className="btn comms-btn " onClick={props.onClickPass} attribsext={{ id: props.id }}>ヽ( `д´*)ノ &times;</Button>
         </div>
     </div>
 
