b0268c338d95ee36b752c5afd9cc2ba0b3ecb996
jsbryce1 <jedi1991@hotmail.com>
2018-10-06 00:06:02 -0600

    final Changes for Jared, I love you Craig, get some sleep

--- client/src/components/Giffy/GifItem.js
+++ client/src/components/Giffy/GifItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
+import "./giffy.css";
 const GifItem = (image) => {
     console.log(image.gif);
     return (
         <li>
             <div className="gif-item">
-            <div>hi</div>
-                <img src={image.gif.data.data[0].images.downsized.url} alt="" />
+                <img className="image-gif" src={image.gif.data.data[0].images.downsized.url} alt="" />
             </div>
         </li>
     )
